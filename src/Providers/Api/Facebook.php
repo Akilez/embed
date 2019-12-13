ce556b3a7dd0dea4062e45d16b945d9c6230d9b0
Michael Christensen <mchristensen@themonastery.org>
2019-12-12 16:32:22 -0800

    Fixed Facebook Embeds

--- src/Providers/Api/Facebook.php
+++ src/Providers/Api/Facebook.php
@@ -133,7 +133,7 @@ class Facebook extends Provider implements ProviderInterface
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&version=v2.3";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
